7aab958db9f048ced102f950e59470fad0f1ddbc
SeoYoung <seoyoung7623@naver.com>
2024-01-22 17:56:31 +0900

    init

---
+++ src/main/java/com/example/gojipserver/GojipServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gojipserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojipServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GojipServerApplication.class, args);
+    }
+
+}
